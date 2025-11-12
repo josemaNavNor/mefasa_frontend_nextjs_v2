31a264cddcd5cdaf7a51ee07b7abeb505f2f6f39
josemaNavNor <navvajosen@gmail.com>
2025-11-12 10:24:40 -0700

    feat: Update logo alt text for better accessibility

--- src/components/img-logo.tsx
+++ src/components/img-logo.tsx
@@ -2,7 +2,7 @@ export default function ImgLogo() {
     return (
         <img
             src="/LOGOMEFASA1.png"
-            alt="Logo"
+            alt="Logo de Mefasa"
             width={100}
             height={80}
         />
